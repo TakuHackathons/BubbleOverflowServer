0062512fb4c9c22ac3d6bc49a669a40e4da0526e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-01-26 12:32:16 +0900

    clientと通信する基本ができた

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { unpack, pack } from 'msgpackr';
 import { Hono } from 'hono'
 import { upgradeWebSocket } from 'hono/cloudflare-workers'
 
@@ -7,13 +8,41 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/msgpack', (c) => {
+  console.log("msgpackRequest");
+  const samplePack = {hello: 'msg'}
+  return c.body(pack(samplePack))
+});
+
+app.get('/room/create', (c) => {
+  console.log("msgpackRequest");
+  const samplePack = {hello: 'msg'}
+  return c.body(pack(samplePack))
+});
+
+app.get('/room/join', (c) => {
+  console.log("msgpackRequest");
+  const samplePack = {hello: 'msg'}
+  return c.body(pack(samplePack))
+});
+
 app.get(
   '/ws',
   upgradeWebSocket((c) => {
     return {
       onMessage(event, ws) {
-        console.log(`Message from client: ${event.data}`)
-        ws.send('Hello from server!')
+        const parsedObj = JSON.parse(event.data);
+        console.log(parsedObj)
+        if (parsedObj.action === 'connect') {
+          console.log('sendConnectMessage')
+          const connectUserMessageObj = {
+            action: "connected",
+            data: {
+              userId: "hogehoge",
+            }
+          }
+          ws.send(JSON.stringify(connectUserMessageObj))
+        }
       },
       onClose: () => {
         console.log('Connection closed')
