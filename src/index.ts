1bf308d60719d4bb4ac03b3bee5303b65a2c7d9a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-01-26 15:13:00 +0900

    roomの作成とゲームのスタート処理の作成

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { unpack, pack } from 'msgpackr';
 import crypto from "crypto";
+import _ from 'lodash';
 import { Hono } from 'hono'
+import { WSContext } from 'hono/ws';
 import { upgradeWebSocket } from 'hono/cloudflare-workers'
+
+const connections: WSContext<WebSocket>[] = [];
+const userIds: string[] = [];
+const roomIdUserIds: {[s: string]: string[]} = {};
 
 const app = new Hono()
 
@@ -34,7 +40,9 @@ app.get(
       onMessage(event, ws) {
         const parsedObj = JSON.parse(event.data);
         if (parsedObj.action === 'connect') {
-          const userId = parsedObj.data.userId || crypto.randomUUID();
+          const userId: string = parsedObj.data.userId || crypto.randomUUID();
+          userIds.push(userId);
+          connections.push(ws);
           const connectUserMessageObj = {
             action: "connected",
             data: {
@@ -42,11 +50,68 @@ app.get(
             }
           }
           ws.send(JSON.stringify(connectUserMessageObj))
+        } else if (parsedObj.action === 'joinRoom') {
+          const userId: string = parsedObj.data.userId;
+          const roomId: string = parsedObj.data.roomId;
+          const roomUsers = roomIdUserIds[roomId] || [];
+          roomUsers.push(userId);
+          if (roomUsers.length >= 4) {
+            const connectedWs = [];
+            const startGameMessageObjs = [];
+            const willRemoveUserIds: string[] = [];
+            for(let i = 0;i < roomUsers.length;++i) {
+              const userId = roomUsers[i];
+              const uidIndex = userIds.findIndex((uid) => uid === userId);
+              if (uidIndex < 0) {
+                willRemoveUserIds.push(userId);
+                break;
+              }
+              const startGameMessageObj = {
+                action: "startGame",
+                data: {
+                  roomId: roomId,
+                  userId: userId,
+                  roomUserIds: roomUsers,
+                  playerNumber: uidIndex + 1,
+                }
+              }
+              connectedWs.push(connections[uidIndex]);
+              startGameMessageObjs.push(startGameMessageObj);
+            }
+
+            if (willRemoveUserIds.length > 0) {
+              _.remove(roomUsers, (roomUserId) => {
+                return willRemoveUserIds.includes(roomUserId);
+              });
+            } else {
+              for(let i = 0;i < connectedWs.length;++i) {
+                connectedWs[i].send(JSON.stringify(startGameMessageObjs[i]));
+              }
+            }
+            roomIdUserIds[roomId] = roomUsers;
+          }
+        } else if (parsedObj.action === 'createRoom') {
+          const userId: string = parsedObj.data.userId;
+          const roomId: string = crypto.randomUUID();
+          roomIdUserIds[roomId] = [userId];
+          const createdRoomMessageObj = {
+            action: "createdRoom",
+            data: {
+              userId: userId,
+              roomId: roomId,
+            }
+          }
+          ws.send(JSON.stringify(createdRoomMessageObj))
         }
       },
       onClose: (event, ws) => {
-        console.log('Connection closed');
-        ws.close();
+        _.remove(connections, (connection, index) => {
+          const isSame = connection === ws;
+          if (isSame) {
+            userIds.splice(index, 1)
+          }
+          return isSame;
+        });
       },
     }
   })
