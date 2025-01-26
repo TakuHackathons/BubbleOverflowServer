75399b819dabdc70acfa38fe1b15c92df0d66e82
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-01-26 15:16:02 +0900

    add code formatter and execute.

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
 import { unpack, pack } from 'msgpackr';
-import crypto from "crypto";
+import crypto from 'crypto';
 import _ from 'lodash';
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 import { WSContext } from 'hono/ws';
-import { upgradeWebSocket } from 'hono/cloudflare-workers'
+import { upgradeWebSocket } from 'hono/cloudflare-workers';
 
 const connections: WSContext<WebSocket>[] = [];
 const userIds: string[] = [];
-const roomIdUserIds: {[s: string]: string[]} = {};
+const roomIdUserIds: { [s: string]: string[] } = {};
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
 app.get('/msgpack', (c) => {
-  console.log("msgpackRequest");
-  const samplePack = {hello: 'msg'}
-  return c.body(pack(samplePack))
+  console.log('msgpackRequest');
+  const samplePack = { hello: 'msg' };
+  return c.body(pack(samplePack));
 });
 
 app.get('/room/create', (c) => {
-  console.log("msgpackRequest");
-  const samplePack = {hello: 'msg'}
-  return c.body(pack(samplePack))
+  console.log('msgpackRequest');
+  const samplePack = { hello: 'msg' };
+  return c.body(pack(samplePack));
 });
 
 app.get('/room/join', (c) => {
-  console.log("msgpackRequest");
-  const samplePack = {hello: 'msg'}
-  return c.body(pack(samplePack))
+  console.log('msgpackRequest');
+  const samplePack = { hello: 'msg' };
+  return c.body(pack(samplePack));
 });
 
 app.get(
@@ -44,12 +44,12 @@ app.get(
           userIds.push(userId);
           connections.push(ws);
           const connectUserMessageObj = {
-            action: "connected",
+            action: 'connected',
             data: {
               userId: userId,
-            }
-          }
-          ws.send(JSON.stringify(connectUserMessageObj))
+            },
+          };
+          ws.send(JSON.stringify(connectUserMessageObj));
         } else if (parsedObj.action === 'joinRoom') {
           const userId: string = parsedObj.data.userId;
           const roomId: string = parsedObj.data.roomId;
@@ -59,7 +59,7 @@ app.get(
             const connectedWs = [];
             const startGameMessageObjs = [];
             const willRemoveUserIds: string[] = [];
-            for(let i = 0;i < roomUsers.length;++i) {
+            for (let i = 0; i < roomUsers.length; ++i) {
               const userId = roomUsers[i];
               const uidIndex = userIds.findIndex((uid) => uid === userId);
               if (uidIndex < 0) {
@@ -67,14 +67,14 @@ app.get(
                 break;
               }
               const startGameMessageObj = {
-                action: "startGame",
+                action: 'startGame',
                 data: {
                   roomId: roomId,
                   userId: userId,
                   roomUserIds: roomUsers,
                   playerNumber: uidIndex + 1,
-                }
-              }
+                },
+              };
               connectedWs.push(connections[uidIndex]);
               startGameMessageObjs.push(startGameMessageObj);
             }
@@ -84,7 +84,7 @@ app.get(
                 return willRemoveUserIds.includes(roomUserId);
               });
             } else {
-              for(let i = 0;i < connectedWs.length;++i) {
+              for (let i = 0; i < connectedWs.length; ++i) {
                 connectedWs[i].send(JSON.stringify(startGameMessageObjs[i]));
               }
             }
@@ -95,26 +95,26 @@ app.get(
           const roomId: string = crypto.randomUUID();
           roomIdUserIds[roomId] = [userId];
           const createdRoomMessageObj = {
-            action: "createdRoom",
+            action: 'createdRoom',
             data: {
               userId: userId,
               roomId: roomId,
-            }
-          }
-          ws.send(JSON.stringify(createdRoomMessageObj))
+            },
+          };
+          ws.send(JSON.stringify(createdRoomMessageObj));
         }
       },
       onClose: (event, ws) => {
         _.remove(connections, (connection, index) => {
           const isSame = connection === ws;
           if (isSame) {
-            userIds.splice(index, 1)
+            userIds.splice(index, 1);
           }
           return isSame;
         });
       },
-    }
-  })
-)
+    };
+  }),
+);
 
-export default app
+export default app;
